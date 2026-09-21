6f48703ca7b57e52fb6d901f06a0486ee6670c25
Shotabdi Das Shormi <shotabdi.das.shormi@gmail.com>
2026-09-21 14:17:28 +0600

    Add files via upload

---
+++ Day 3/YourWork/default value.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v ;
+    v.push_back(5);
+    v.push_back(10);
+
+    for(int x : v) {
+        cout << x << " ";
+    }
+    cout << "\n";
+    
+    return 0;
+}
